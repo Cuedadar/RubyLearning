e26558d9166cbb0c97bfbdf697c2ca48c402be45
John King <king.john.t@gmail.com>
2018-08-06 18:11:44 -0700

    Initial Commit: First Couple of Lessons

---
+++ Section 2/section2_17.rb
@@ -0,0 +1,15 @@
+# This is a comment
+
+puts "Hello World" # Prints "Hello World" to the console
+
+# In this section of code,
+# we are going to do operations A, B, and C
+
+=begin
+
+puts 1 + 1
+puts 5 + 3
+puts "Hello World"
+p "Blah blah blah"
+
+=end
